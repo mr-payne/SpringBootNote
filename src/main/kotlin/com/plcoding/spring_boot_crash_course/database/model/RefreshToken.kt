df63e6af06106a21d3fa6e970a59f29f0312c48e
Thomas Hussey <thomashussey@Mac-1570.lan>
2025-07-26 09:47:48 -0400

    get project running and connecting to mongo db

--- src/main/kotlin/com/plcoding/spring_boot_crash_course/database/model/RefreshToken.kt
+++ src/main/kotlin/com/plcoding/spring_boot_crash_course/database/model/RefreshToken.kt
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
-@Document("refresh_tokens")
+@Document("refreshToken")
 data class RefreshToken(
     val userId: ObjectId,
     @Indexed(expireAfter = "0s")
